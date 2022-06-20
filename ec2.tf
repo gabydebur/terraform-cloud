a427f98d607c26f0858742f6417a615d89083532
gabydebur <g.westrelin@gmail.com>
2022-06-20 18:17:39 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-09e26728dcea15e0a"
+  instance_type = "t2.micro"
+  key_name      = "devops-gaby"
+  tags = {
+    name = "ec2-gaby"
+
+  }
+  root_block_device {
+    delete_on_termination = true
+  }
+}
