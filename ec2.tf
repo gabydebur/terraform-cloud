a891acb7246bedb7b028524ad8a35762510a3029
gabydebur <g.westrelin@gmail.com>
2022-06-20 18:56:25 +0200

    Update ec2.tf
    remove tagf

--- ec2.tf
+++ ec2.tf
@@ -6,10 +6,10 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-09e26728dcea15e0a"
   instance_type = "t2.micro"
   key_name      = "devops-gaby"
-  tags = {
-    name = "ec2-gaby"
-
-  }
+#  tags = {
+#    name = "ec2-gaby"
+#
+#  }
   root_block_device {
     delete_on_termination = true
   }
